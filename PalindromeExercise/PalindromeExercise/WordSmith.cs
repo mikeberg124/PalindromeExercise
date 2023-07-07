b76b151df20341d7783cca6485bb6aad6095ef75
Michael Berg <mberg12@bu.edu>
2023-07-07 14:31:03 -0400

    completed palindrome exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+		
+			public static bool IsAPalindrome(string word)
+			{
+			string reversedWord = "";
+			for (int i = word.Length-1; i >= 0; i++)
+			{
+
+				reversedWord += word;
+
+			}
+
+			return word.ToLower() == reversedWord.ToLower();
+			
+			}
+
+
+
+		
+	}
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,27 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("rotator", true)]
+        [InlineData("mom", true)]
+        [InlineData("a", true)]
+        [InlineData("square", false)]
+        [InlineData("sky", false)]
 
+
+        public static void Test1(string testWord, bool expected)
+        {
+            //Arrange- skip if static
+
+            //Act
+            var actual = WordSmith.IsAPalindrome(testWord);
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
